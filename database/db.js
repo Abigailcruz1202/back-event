0c460b4fe8dcc234ef8a5000396877f19ceacf20
Viviana Abigail Cruz <cruzabi994@gmail.com>
2021-09-24 13:47:12 -0300

    make it better

--- database/db.js
+++ database/db.js
@@ -6,15 +6,6 @@ const {
     DB_USER, DB_PASSWORD, DB_HOST,
   } = process.env;
 
-// Option 2: Passing parameters separately (other dialects)
-// const sequelize = new Sequelize(
-//     database.database,
-//     database.username,
-//     database.password, {
-//         host: database.host,
-//         dialect: 'postgres'
-//     }
-// );
 
 const sequelize = new Sequelize(`postgres://zhdfxnao:cXuuyfoyKt9vxEUWiZ4NzEKZIhA2WvRZ@kesavan.db.elephantsql.com/zhdfxnao`, {
     // Hace que no muestre todos los mensajes de conexion con SQL en la consola cuando inicias el servidor.
