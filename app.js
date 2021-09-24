1d9fde56429c4d3a5950f5ce35d78f2ba4e008cd
Viviana Abigail Cruz <cruzabi994@gmail.com>
2021-09-24 01:00:01 -0300

    firts commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = 3001 || process.env.PORT
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
-server.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://event-psi.vercel.app/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
